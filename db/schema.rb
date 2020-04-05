9b4f19b5a10dff2fb427a4c0b43b8435c9904edd
Matt Stephens <mattbastard@gmail.com>
2020-04-05 09:31:47 -0400

    Add Bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_04_04_232021) do
+
+  create_table "queries", force: :cascade do |t|
+    t.string "title"
+    t.text "request"
+    t.text "response"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
