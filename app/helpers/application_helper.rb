c2e4557a35fa535cd4655d7a440259f8b874c690
Matt Stephens <mattbastard@gmail.com>
2020-04-05 18:07:16 -0400

    Add navbar and about page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def nav_link(text,path)
+    if current_page?(path)
+      content_tag :li, class: "nav-items active" do
+        link_to(path, class: "nav-link") do
+          sanitize(text) + content_tag(:span, "(current)", class: "sr-only")
+        end
+      end
+    else
+      content_tag :li, class: "nav-items" do
+        link_to(text, path, class: "nav-link")
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "queries#index"
+  get "about", to: "static_pages#index"
   resources :queries
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
