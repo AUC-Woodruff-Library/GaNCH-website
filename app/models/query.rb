d83a4cc3cf421f37f30efae132e62bf25aa8dda2
Matt Stephens <mattbastard@gmail.com>
2020-04-19 15:22:09 -0400

    Add sparql-client and query

--- app/models/query.rb
+++ app/models/query.rb
@@ -1,4 +1,27 @@
 class Query < ApplicationRecord
+  require "sparql/client"
+  extend ActiveModel::Callbacks
+
   validates :title, presence: true
   validates :request, presence: true
+
+  define_model_callbacks :update
+  before_save :get_query_response
+
+  private
+
+  # send request to wikidata as a query
+  def get_query_response
+    throw :abort if self.request.nil?
+    sparql = SPARQL::Client.new("https://query.wikidata.org/sparql")
+    response = sparql.query(self.request)
+
+    # sparql query will return nil on error
+    self.response = response unless response.nil?
+
+    logger.debug("Wikimedia query response: #{response}")
+
+    return @query
+
+  end
 end
