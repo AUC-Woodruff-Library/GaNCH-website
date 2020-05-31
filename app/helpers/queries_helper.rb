ab69e6a7c03d7f8d28a36d851f90071519b56a2c
Matt Stephens <mattbastard@gmail.com>
2020-05-31 13:49:15 -0400

    break row label out into helper

--- app/helpers/queries_helper.rb
+++ app/helpers/queries_helper.rb
@@ -2,4 +2,13 @@ module QueriesHelper
   def get_user(query)
     @user = User.find(query.user_id)
   end
+
+  def get_row_label(row)
+    field = row.keys.detect { |k| k =~ /Label/i }
+    if row[field]
+      row[field]['value'].to_s
+    else
+      ''
+    end
+  end
 end
