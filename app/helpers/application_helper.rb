411fbeb7e49b218b86623aeb49f081e66046ec49
Matt Stephens <mattbastard@gmail.com>
2020-06-13 12:22:16 -0400

    Logos and text for about page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,8 +16,10 @@ module ApplicationHelper
 
   def title(name, show_title = true)
     @show_title = show_title
-    content_for(:title) do
-      name
+    if @show_title
+      content_for(:title) do
+        name
+      end
     end
   end
 end
