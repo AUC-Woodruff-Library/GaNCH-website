c06984985b93b76671b823f54b3c06199361f9d6
Matt Stephens <mattbastard@gmail.com>
2020-09-05 16:09:47 -0400

    Reopen signups on prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -116,5 +116,5 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   # Allow signups
-  config.permit_signups = 'false'
+  config.permit_signups = 'true'
 end
