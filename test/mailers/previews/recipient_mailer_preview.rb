c98cbaaf8959a2114eec5449c923d26f18385994
Matt Stephens <mattbastard@gmail.com>
2020-09-05 17:11:32 -0400

    Use inline styles for logos in email
    Moving code to a shared template and forcing https to please Google.

--- test/mailers/previews/recipient_mailer_preview.rb
+++ test/mailers/previews/recipient_mailer_preview.rb
@@ -1,6 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/recipient_mailer
 class RecipientMailerPreview < ActionMailer::Preview
   def reminder_email
-    RecipientMailer.with(recipient: Recipient.first).reminder_email
+    RecipientMailer.with(recipient: Recipient.first, user: User.first).reminder_email
   end
 end
